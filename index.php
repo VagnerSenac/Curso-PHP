83d244adbe358ea8e0fa489311cc55c50671955e
Vagner Lourenço <90052483+VagnerSenac@users.noreply.github.com>
2022-11-11 22:18:51 -0300

    11-11

--- index.php
+++ index.php
@@ -1,4 +1,48 @@
 <?php
-echo 'olá mundo';
-?>
+echo 'olá mundo <br>';
+echo 'Vagner Lourenço';
 
+/*
+http://localhost/curso-php/index.php
+*/
+
+print('<h1>escrevendo</h1>');
+
+print_r('<h2>Novo</h2>');
+
+$nome = 'Vagner';
+$idade = 41;
+$altura = 1.82;
+$fumante = true;
+
+echo '<br>';
+echo $nome ;
+echo '<br>';
+echo  $idade;
+echo '<br>';
+echo $altura;
+echo '<br>';
+echo $fumante; 
+echo '<br>';
+
+//concatenação
+echo '<br>';
+echo $nome . '<br>';
+echo  $idade . '<br>';
+echo $altura . '<br>';
+echo $fumante . '<br>';
+
+echo 'Olá ' . $nome . ' você tem ' . $idade. 'anos';
+
+echo '<hr>';
+// pega o tipo da variável
+echo gettype($nome) . '<br>';
+echo gettype($idade) . '<br>';
+echo gettype($altura) . '<br>';
+echo gettype($fumante) . '<br>';
+
+$altura = '1.75';
+echo gettype($altura) . '<br>';
+
+
+?>
